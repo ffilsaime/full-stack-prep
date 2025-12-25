9faee102529707e52be24f6b84566e99fdcffb39
Florebencia <florebencia.filsaime1998@gmail.com>
2025-12-24 19:55:41 -0500

    Started Working On the Header Functional Component

---
+++ eazystore-ui/src/components/Header.tsx
@@ -0,0 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBasket, faTags } from "@fortawesome/free-solid-svg-icons";
+
+export default function Header(){
+
+    return (
+        <div className="header">
+            <div className="container">
+                <a href="/" className="link">
+                    <FontAwesomeIcon icon={faTags} className="fa-icon"/>
+                    <span className="brand-title">Eazy Stickers</span>
+                </a>
+                <nav className="nav">
+                    <ul>
+                        <li>
+                            <a href="/" className="link">Home</a>
+                        </li>
+                        <li>
+                            <a href="/about" className="link">About</a>
+                        </li>
+                        <li>
+                            <a href="/contact" className="link">Contact</a>
+                        </li>
+                        <li>
+                            <a href="/login" className="link">Login</a>
+                        </li>
+                        <li>
+                            <a href="/cart" className="link">
+                                <FontAwesomeIcon icon={faShoppingBasket} />
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+//export default Header;
