a6f57e84920db373274955fcb7219abcb7821a9c
Florebencia <florebencia.filsaime1998@gmail.com>
2026-01-31 12:13:18 -0500

    Implemented How Eazystore UI interacts with the Backend
    - I used axios to do http requests
    - I also learned a bit more about useEffect and useState

--- eazystore-ui/src/components/Home.tsx
+++ eazystore-ui/src/components/Home.tsx
@@ -1,8 +1,22 @@
 import PageHeading from "./PageHeading.tsx";
 import ProductListings from "./ProductListings.tsx";
-import products from "../data/products/Products.ts";
+//import products from "../data/products/Products.ts";
+import apiClient from "../api/apiClient.ts";
+import {useState, useEffect} from "react";
 
 export default function Home(){
+    const [products, setProducts] = useState([]);
+
+    // Run once when the component mounts
+    // Mounting is the process of creating and adding the component into DOM
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    const fetchProducts = async () => {
+        const response = await apiClient.get("/products"); //Axios get request
+        setProducts(response.data);
+    };
 
     return (
         <div className="home-container">
