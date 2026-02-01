651f8424d8a728d38eec8f81c668a8d5053e8d23
Florebencia <florebencia.filsaime1998@gmail.com>
2026-02-01 10:57:52 -0500

    Created A Cors Configuration Instead of Using the Annotation

--- eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
+++ eazystore/src/main/java/com/eazybytes/eazystore/controller/ProductController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
-@CrossOrigin(origins = "http://localhost:5173")
+//@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
 
     private final ProductService productService;
